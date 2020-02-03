124810bc88e8b3e33e06866cc5d64d1e4e077ea9
Palash Lambhate <Palash144@gmail.com>
2020-02-03 16:49:20 +0530

    s

---
+++ src/main/java/io/ftpdevelopers/hawkeye/EmailDemoApplication.java
@@ -0,0 +1,33 @@
+package io.ftpdevelopers.hawkeye;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@SpringBootApplication
+public class EmailDemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		sendMail();
+	}
+
+	public void sendMail() {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo("palash144@gmail.com");
+		//message.setFrom("shalini@jc.com");
+		message.setSubject("Welcome!!");
+		message.setText("Hello Friends chai peelo");
+		javaMailSender.send(message);
+	}
+}
